592527c68f194cd5b51ba276bb281e05f350ec9c
Robert Hjalmers <robert.hjalmers@gmail.com>
2016-12-10 10:22:53 +0100

    fixed release

--- src/generic-table/interfaces/gt-config.ts
+++ src/generic-table/interfaces/gt-config.ts
@@ -1,6 +1,5 @@
 import { GtConfigField } from './gt-config-field';
 import { GtConfigSetting } from './gt-config-setting';
-import { GtPagingInfo } from './gt-paging-info';
 import { GtRow } from './gt-row';
 import {GtInformation} from './gt-information';
 
