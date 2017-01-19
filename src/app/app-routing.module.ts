96737ca59d9af88c7fa9768c88ecfc2a1e0bc929
Robert Hjalmers <robert.hjalmers@gmail.com>
2017-01-19 16:58:45 +0100

    changed baseUrl

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,11 +9,13 @@ import { RestComponent } from './rest/rest.component';
 import { StaticComponent } from './basic/basic.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'examples', pathMatch: 'full' },
+  { path: '', redirectTo: '/examples', pathMatch: 'full' },
   { path: 'examples', component: ExamplesComponent },
   { path: 'lazy', component: LazyComponent },
   { path: 'rest', component: RestComponent },
   { path: 'static', component: StaticComponent },
+  { path: '**', component: ExamplesComponent }
+
 ];
 
 @NgModule({
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: '/angular2-generic-table/'
 };
