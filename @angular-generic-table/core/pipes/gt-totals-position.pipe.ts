4289c2b9e80f891507857718584da0c8cc5ca278
Robert Hjalmers <robert.hjalmers@gmail.com>
2017-07-20 09:20:59 +0200

    fixed tslint error

--- @angular-generic-table/core/pipes/gt-totals-position.pipe.ts
+++ @angular-generic-table/core/pipes/gt-totals-position.pipe.ts
@@ -1,11 +1,12 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import {GtConfigTotal} from "../interfaces/gt-config-total";
 
 @Pipe({
   name: 'gtTotalsPosition'
 })
 export class GtTotalsPositionPipe implements PipeTransform {
 
-  transform(array: any, position: 'header' | 'footer' = 'header'): Array<any> {
+  transform(array: Array<GtConfigTotal>, position: 'header' | 'footer' = 'header'): Array<any> {
     return array.filter(total=>{
 
       // if no position is defined assume position to be header
