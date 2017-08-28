d66b55806a501ec0338f28c63c6f1099d2ffcff1
hjalmers <robert.hjalmers@gmail.com>
2017-08-28 16:50:43 +0200

    fixed lint errors

--- @angular-generic-table/core/pipes/gt-chunk.pipe.ts
+++ @angular-generic-table/core/pipes/gt-chunk.pipe.ts
@@ -8,17 +8,21 @@ export class GtChunkPipe implements PipeTransform {
 
   transform(array: any[], gtInfo: GtInformation, chunkSize: number, page: number, refreshPageArray: boolean, refreshData: number, gtEvent: EventEmitter<any>, data: {exportData: Array<any>}): any[] {
 
-    if (!Array.isArray(array)) return array;
+    if (!Array.isArray(array)) { return array; }
     data.exportData = array; // store data for export
 
-    const pages = [];
-    for (let i = 0, len = array.length; i < len; i += chunkSize)
-      pages.push(array.slice(i, i + chunkSize));
-    gtInfo.pageTotal = pages.length;
+    const PAGES = [];
+    const ENTRIES = array.length;
+
+    for (let i = 0; i < ENTRIES; i += chunkSize) {
+        PAGES.push(array.slice(i, i + chunkSize));
+    }
+
+    gtInfo.pageTotal = PAGES.length;
     setTimeout(() => gtEvent.emit({
       name: 'gt-info',
       value: gtInfo
     }), 0);
-    return pages[page - 1];
+    return PAGES[page - 1];
   }
 }
