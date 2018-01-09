435371b2c7201628e4ab1ec6f695be467a7cde5f
Robert Hjalmers <robert.hjalmers@gmail.com>
2018-01-09 16:18:21 +0100

    fixed #169

--- src/app/rest/rest.component.ts
+++ src/app/rest/rest.component.ts
@@ -5,148 +5,162 @@ import {CustomRowComponent} from '../custom-row/custom-row.component';
 import {GenericTableComponent, GtConfig} from '@angular-generic-table/core';
 import 'rxjs/add/operator/map';
 
+export interface RowData {
+    id: number;
+    name: string;
+    email: string;
+    gender: string;
+    favorite_color: string;
+}
+
 @Component({
-  selector: 'app-rest',
-  templateUrl: './rest.component.html'
+    selector: 'app-rest',
+    templateUrl: './rest.component.html'
 })
 export class RestComponent implements OnInit {
 
-  public configObject: GtConfig<any>;
+    public configObject: GtConfig<any>;
 
-  @Output() data = new EventEmitter();
+    @Output() data = new EventEmitter();
 
-  @ViewChild(GenericTableComponent)
-  private myTable: GenericTableComponent<any, CustomRowComponent>;
-  public showColumnControls = false;
-  public selectedRows = 0;
-  private url = 'https://private-730c61-generictable.apiary-mock.com/data'; // apiary end point
+    @ViewChild(GenericTableComponent)
+    private myTable: GenericTableComponent<RowData, CustomRowComponent>;
+    public showColumnControls = false;
+    public selectedRows = 0;
+    private url = 'https://private-730c61-generictable.apiary-mock.com/data'; // apiary end point
 
-  constructor(private http: HttpClient) {
+    constructor(private http: HttpClient) {
 
-    this.configObject = {
-      settings: [{
-        objectKey: 'id',
-        visible: true,
-        sort: 'desc',
-        columnOrder: 0,
-        enabled: true
+        this.configObject = {
+            settings: [{
+                objectKey: 'id',
+                visible: true,
+                sort: 'desc',
+                columnOrder: 0,
+                enabled: true
 
-      }, {
-        objectKey: 'name',
-        visible: true,
-        sort: 'enable',
-        columnOrder: 1
-      }, {
-        objectKey: 'email',
-        visible: true,
-        enabled: true,
-        sort: 'enable',
-        sortOrder: 0,
-        columnOrder: 2
-      }, {
-        objectKey: 'gender',
-        visible: true,
-        enabled: true,
-        sort: 'enable',
-        sortOrder: 0,
-        columnOrder: 3,
-        search: false
-      }, {
-        objectKey: 'favorite_color',
-        visible: true,
-        enabled: true,
-        sort: 'disable',
-        sortOrder: 0,
-        columnOrder: 4,
-        search: false
-      }],
-      fields: [{
-        name: 'Id',
-        objectKey: 'id',
-        columnClass: 'clickable sort-numeric',
-        expand: {
-          component: CustomRowComponent
-        }
-      }, {
-        name: 'Name',
-        objectKey: 'name',
-        columnClass: 'sort-string',
-        value: function(row){return row.first_name + ' ' + row.last_name; },
-        render: function(row){return '<div>' + row.first_name + ' ' + row.last_name + '</div>'; }
-      }, {
-        name: 'Favorite color',
-        objectKey: 'favorite_color',
-        columnClass: 'text-right',
-        render: function(row){return '<div style="float:right;width:15px;height:15px;border-radius:50%;background: ' + row.favorite_color + '"></div>'; },
-        click: (row) => {return console.log(row.first_name + '\'s favorite color is: ' + row.favorite_color ); }
-      }, {
-        name: 'Gender',
-        columnClass: 'sort-string',
-        objectKey: 'gender'
-      }, {
-        name: 'Email',
-        columnClass: 'sort-string',
-        objectKey: 'email',
-        render: function(row){return '<a href="mailto:' + row.email + '">' + row.email + '</a>'; }
-      }],
-      data: []
-    };
-  }
-
-  public getData = function() {
-
-    // tell generic table instance that we're loading data
-    this.myTable ? this.myTable.loading = true : '';
-
-    this.http.get(this.url)
-        //.map((res: Response) => res.json())
-        .subscribe(res => {
-          this.configObject.data = res.data;
-        });
-  };
-
-  public addData = function() {
-
-    // create mock data
-    const random = Math.floor(Math.random() * this.configObject.data.length - 1) + 1;
-    const firstName = this.configObject.data[random].first_name;
-    const lastName = this.configObject.data[Math.floor(Math.random() * this.configObject.data.length - 1) + 1].last_name;
-    const gender = this.configObject.data[random].gender;
-    const favoriteColor = this.configObject.data[Math.floor(Math.random() * this.configObject.data.length - 1) + 1].favorite_color;
-    const birthday = this.configObject.data[Math.floor(Math.random() * this.configObject.data.length - 1) + 1].birthday;
-
-    // push data to data array (could be swapped to a method for persisting the data to a database).
-    this.configObject.data.push({
-      'id': this.configObject.data.length + 1,
-      'first_name': firstName,
-      'last_name': lastName,
-      'email': firstName + '.' + lastName + '@some_email_address.xyz',
-      'gender': gender,
-      'favorite_color': favoriteColor,
-      'birthday': birthday
-    });
-  };
-
-
-  /** Apply predefined filter using first_name.
-   * */
-  public applyFilter = function(){
-    this.myTable.gtApplyFilter({
-      first_name: ['Victor', 'Joe', 'Carol']
-    });
-  };
-
-  /** Listen for events
-   * */
-  public trigger = function($event){
-    console.log($event);
-    if ($event.value && $event.value.selectedRows) {
-      this.selectedRows = $event.value.selectedRows.length;
+            }, {
+                objectKey: 'name',
+                visible: true,
+                sort: 'enable',
+                columnOrder: 1
+            }, {
+                objectKey: 'email',
+                visible: true,
+                enabled: true,
+                sort: 'enable',
+                sortOrder: 0,
+                columnOrder: 2
+            }, {
+                objectKey: 'gender',
+                visible: true,
+                enabled: true,
+                sort: 'enable',
+                sortOrder: 0,
+                columnOrder: 3,
+                search: false
+            }, {
+                objectKey: 'favorite_color',
+                visible: true,
+                enabled: true,
+                sort: 'disable',
+                sortOrder: 0,
+                columnOrder: 4,
+                search: false
+            }],
+            fields: [{
+                name: 'Id',
+                objectKey: 'id',
+                columnClass: 'clickable sort-numeric',
+                expand: {
+                    component: CustomRowComponent
+                }
+            }, {
+                name: 'Name',
+                objectKey: 'name',
+                columnClass: 'sort-string',
+                value: function(row){return row.first_name + ' ' + row.last_name; },
+                render: function(row){return '<div>' + row.first_name + ' ' + row.last_name + '</div>'; }
+            }, {
+                name: 'Favorite color',
+                objectKey: 'favorite_color',
+                columnClass: 'text-right',
+                render: function(row){return '<div style="float:right;width:15px;height:15px;border-radius:50%;background: ' + row.favorite_color + '"></div>'; },
+                click: (row) => {return console.log(row.first_name + '\'s favorite color is: ' + row.favorite_color ); }
+            }, {
+                name: 'Gender',
+                columnClass: 'sort-string',
+                objectKey: 'gender'
+            }, {
+                name: 'Email',
+                columnClass: 'sort-string',
+                objectKey: 'email',
+                render: function(row){return '<a href="mailto:' + row.email + '">' + row.email + '</a>'; }
+            }],
+            data: []
+        };
     }
-  };
 
-  ngOnInit() {
-    this.getData();
-  }
+    public getData = function() {
+
+        // tell generic table instance that we're loading data
+        this.myTable ? this.myTable.loading = true : '';
+
+        this.http.get(this.url)
+        //.map((res: Response) => res.json())
+            .subscribe(res => {
+                this.configObject.data = res.data;
+            });
+    };
+
+    public addData = function() {
+        // create mock data
+        const random = Math.floor(Math.random() * this.configObject.data.length - 1) + 1;
+        const firstName = this.configObject.data[random].first_name;
+        const lastName = this.configObject.data[Math.floor(Math.random() * this.configObject.data.length - 1) + 1].last_name;
+        const gender = this.configObject.data[random].gender;
+        const favoriteColor = this.configObject.data[Math.floor(Math.random() * this.configObject.data.length - 1) + 1].favorite_color;
+
+        // add data to table (could be swapped to a method for persisting the data to a database i.e. should go through server first).
+        // gtAdd method returns new data array with added records, which we reassign to this.configObject.data
+        this.configObject.data = this.myTable.gtAdd([{
+            'id': this.configObject.data.length + 1,
+            'first_name': firstName,
+            'last_name': lastName,
+            'email': firstName + '.' + lastName + '@some_email_address.xyz',
+            'gender': gender,
+            'favorite_color': favoriteColor
+        }]);
+    };
+
+    public deleteData = function() {
+        this.configObject.data = this.myTable.gtDelete('id', this.configObject.data[0].id);
+    };
+
+    public deleteAll = function() {
+        this.configObject.data = this.myTable.gtDelete('gender', 'Male', 'all');
+    };
+
+
+    /** Apply predefined filter using first_name.
+     * */
+    public applyFilter = function(){
+        this.myTable.gtApplyFilter({
+            first_name: ['Victor', 'Joe', 'Carol']
+        });
+    };
+
+    /** Listen for events
+     * */
+    public trigger = function($event){
+        console.log($event);
+        if ($event.value && $event.value.selectedRows) {
+            this.selectedRows = $event.value.selectedRows.length;
+        }
+    };
+
+    ngOnInit() {
+        this.getData();
+    }
 
 }
