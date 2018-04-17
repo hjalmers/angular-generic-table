358f63413c79f393510450a3f046e9ef550c38fa
Matthew Klein <mklein994@gmail.com>
2018-04-17 13:30:23 -0500

    Add a mock generic table example for column search

--- src/app/column-search/column-search.component.ts
+++ src/app/column-search/column-search.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { GenericTableComponent, GtConfig } from '@angular-generic-table/core';
+
+import { Person, PEOPLE } from './person';
 
 @Component({
 	selector: 'column-search',
@@ -6,7 +9,48 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./column-search.component.scss']
 })
 export class ColumnSearchComponent implements OnInit {
-	constructor() {}
+	public configObject: GtConfig<Person>;
+
+	constructor() {
+		this.configObject = {
+			settings: [
+				{
+					objectKey: 'id',
+					sort: 'asc',
+					sortOrder: 1,
+					columnOrder: 0
+				},
+				{
+					objectKey: 'name',
+					sort: 'asc',
+					sortOrder: 0,
+					columnOrder: 1
+				},
+				{
+					objectKey: 'lucky_number',
+					sort: 'enable',
+					columnOrder: 2,
+					visible: true
+				}
+			],
+			fields: [
+				{
+					name: 'Id',
+					objectKey: 'id'
+				},
+				{
+					name: 'Name',
+					objectKey: 'name'
+				},
+				{
+					name: 'Lucky number',
+					objectKey: 'lucky_number',
+					stackedHeading: 'Custom heading'
+				}
+			],
+			data: PEOPLE
+		};
+	}
 
 	ngOnInit() {}
 }
