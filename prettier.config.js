7ce831808d3d88d11ca54b448817380f389c91af
Robert Hjalmers <robert.hjalmers@gmail.com>
2018-04-22 11:15:56 +0200

    updated prettier and tslint config

--- prettier.config.js
+++ prettier.config.js
@@ -2,13 +2,34 @@
 // If you don't specify one of these properties,
 // the default value will be applied.
 module.exports = {
-  printWidth: 80,
-  tabWidth: 2,
-  useTabs: true,
-  semi: true,
-  singleQuote: true,
-  trailingComma: 'none', // other options `es5` or `all`
-  bracketSpacing: true,
-  arrowParens: 'avoid', // other option 'always'
-  parser: 'typescript'
+	printWidth: 80,
+	tabWidth: 2,
+	useTabs: true,
+	semi: true,
+	singleQuote: true,
+	trailingComma: 'none', // other options `es5` or `all`
+	bracketSpacing: true,
+	arrowParens: 'avoid', // other option 'always'
+	parser: 'typescript',
+	overrides: [
+		{
+			files: '**/*.json',
+			options: {
+				parser: 'json',
+				singleQuote: false
+			}
+		},
+		{
+			files: '**/*.scss',
+			options: {
+				parser: 'css'
+			}
+		},
+		{
+			files: '**/*.md',
+			options: {
+				parser: 'markdown'
+			}
+		}
+	]
 };
