157656d6c5cbb7c54494dd39839a3aff7e7992bc
Robert Hjalmers <robert.hjalmers@gmail.com>
2018-04-23 19:42:40 +0200

    dynamic menu creation

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DemoContentService } from '../shared/services/demo-content.service';
 
 @Component({
 	selector: 'menu',
@@ -6,6 +8,23 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
-	constructor() {}
-	ngOnInit() {}
+	sections: Array<any>;
+	constructor(
+		public router: Router,
+		private demoContentService: DemoContentService
+	) {}
+	ngOnInit() {
+		this.sections = this.router.config
+			.map(route => {
+				const NAME = route.path.replace(/-/g, ' ');
+				return {
+					title: NAME.charAt(0).toUpperCase() + NAME.slice(1),
+					path: route.path,
+					examples: this.demoContentService
+						.getExamples(route.path)
+						.sections.map(section => section)
+				};
+			})
+			.filter(section => section.examples.length > 0);
+	}
 }
