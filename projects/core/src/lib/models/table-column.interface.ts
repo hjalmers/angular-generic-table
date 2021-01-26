252f3e9c4396c3c65aa4cd80fed655e5afaf69de
Robert Hjalmers <robert.hjalmers@gmail.com>
2021-01-26 09:02:45 +0100

    feat: add ability to set header for mobile view

--- projects/core/src/lib/models/table-column.interface.ts
+++ projects/core/src/lib/models/table-column.interface.ts
@@ -2,6 +2,7 @@ import { TemplateRef } from '@angular/core';
 
 export interface TableColumn {
   header?: string;
+  mobileHeader?: string | boolean;
   hidden?: boolean;
   sortable?: boolean;
   order?: number;
