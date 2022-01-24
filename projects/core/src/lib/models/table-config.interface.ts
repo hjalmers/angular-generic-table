c71ea2100ddb06ce82edd3297fdd37d00b1b8e31
Hjalmers <opensource@rhj.se>
2022-01-24 12:54:58 +0100

    fix(mobile-layout): add config option and improve layout

--- projects/core/src/lib/models/table-config.interface.ts
+++ projects/core/src/lib/models/table-config.interface.ts
@@ -2,6 +2,7 @@ import { TableColumn } from './table-column.interface';
 
 export interface TableConfig {
   hidden?: boolean;
+  mobileLayout?: boolean;
   class?: string;
   rows?: {
     [key: string]: TableColumn;
