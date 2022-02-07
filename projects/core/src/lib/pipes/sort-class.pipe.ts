96382ef36fae2572796d1379ed8a85c8be08557f
Hjalmers <opensource@rhj.se>
2022-02-07 17:10:50 +0100

    fix(a11y): set aria-sort attribute on sorted column

--- projects/core/src/lib/pipes/sort-class.pipe.ts
+++ projects/core/src/lib/pipes/sort-class.pipe.ts
@@ -5,7 +5,13 @@ import { Order } from '../enums/order.enum';
   name: 'sortClass',
 })
 export class SortClassPipe implements PipeTransform {
-  transform(selection: { sortBy: string; sortByOrder: Order } | any, property: string): string {
-    return selection?.sortBy === property ? 'sort-' + selection.sortByOrder : '';
+  transform(selection: { sortBy: string; sortByOrder: Order } | any, property: string, aria = false): string | null {
+    return selection?.sortBy === property
+      ? !aria
+        ? 'sort-' + selection.sortByOrder
+        : selection.sortByOrder + 'ending'
+      : !aria
+      ? ''
+      : null;
   }
 }
