14af9bcd1c6b932b5b7bd6261e286a66c3837f68
Hjalmers <opensource@rhj.se>
2022-07-31 14:02:34 +0200

    docs: add support for theming

---
+++ .storybook/manager.js
@@ -0,0 +1,5 @@
+// import scss using scss-loader file
+import "!style-loader!css-loader!postcss-loader!sass-loader!./manager.scss";
+
+// set default theme
+document.body.setAttribute("data-theme", "theme-bootstrap");
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,6 @@
 import { setCompodocJson } from "@storybook/addon-docs/angular";
 import docJson from "../documentation.json";
+
 setCompodocJson(docJson);
 
 export const parameters = {
@@ -11,4 +12,21 @@ export const parameters = {
     },
   },
   docs: { inlineStories: true },
+  themes: {
+    //default: "Swimbird - Dark",
+    default: "Bootstrap",
+    clearable: false,
+    list: [
+      //{ name: "Swimbird - Dark", class: "theme-sb-dark", color: "#00aced" },
+      //{ name: "Swimbird - Light", class: "theme-sb-light", color: "#00aced" },
+      {
+        name: "Bootstrap",
+        class: "theme-bootstrap",
+        color: "var(--bs-primary)",
+      },
+    ],
+    onChange: (theme) => {
+      document.body.setAttribute("data-theme", theme.class);
+    },
+  },
 };
