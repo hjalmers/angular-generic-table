bd030092df5ab53750b12dc9a9d6c956fd985f4d
Hjalmers <opensource@rhj.se>
2022-08-05 12:23:48 +0200

    docs: add logo

---
+++ .storybook/theme.js
@@ -0,0 +1,9 @@
+import { create } from "@storybook/theming";
+
+export default create({
+  base: "light",
+  brandTitle: "Angular Generic Table",
+  brandUrl: "https://github.com/hjalmers/angular-generic-table",
+  brandImage: "logo.svg",
+  brandTarget: "_self",
+});
