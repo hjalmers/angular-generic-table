c30b0dc4417484c3d8d538c1bc88a00123208bb1
Hjalmers <opensource@rhj.se>
2023-01-24 07:29:53 +0100

    docs(footer): update

--- projects/docs/src/app/examples/footer/simple.snippets.ts
+++ projects/docs/src/app/examples/footer/simple.snippets.ts
@@ -1,7 +1,7 @@
 export const SIMPLE_SNIPPETS = [
   {
-    name: 'basic-table.component.ts',
-    code: `import { Component } from '@angular/core';
+    name: 'footer-table.component.ts',
+    code: `import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { TableConfig, TableRow } from '@angular-generic-table/core';
 import { DecimalPipe } from '@angular/common';
 
@@ -11,9 +11,17 @@ import { DecimalPipe } from '@angular/common';
       <angular-generic-table [data]="data" [config]="config">
       </angular-generic-table>
     </div>
+    <ng-template #heightTmplRef let-row="row" let-col="col">
+      <ng-container *ngIf="row[col.key || col] as height"
+        >{{ height }} m
+      </ng-container>
+    </ng-template>
   \`,
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  @ViewChild('heightTmplRef', { static: true }) heightTmplRef:
+    | TemplateRef<any>
+    | undefined;
   data = [
     {
       firstName: 'Peter',
@@ -22,6 +30,7 @@ export class FooterComponent {
       favoriteFood: 'Pasta',
       age: 27,
       weight: 85.457,
+      height: 1.85,
     },
     {
       firstName: 'Mary Jane',
@@ -30,92 +39,106 @@ export class FooterComponent {
       favoriteFood: 'Pizza',
       age: 25,
       weight: 60.123,
+      height: 1.65,
     },
   ];
-  config: TableConfig = {
-    columns: {
-      firstName: {},
-      lastName: {},
-      gender: {},
-      favoriteFood: {
-        class: 'text-end flex-end',
-      },
-      age: {
-        class: 'text-end',
-      },
-      weight: {
-        class: 'text-end',
-        transform: {
-          pipe: DecimalPipe,
-          args: ['1.0-2'],
-        },
-      },
-    },
-    footer: {
-      headers: {
-        sum: 'Total',
-        numberOfWomen: 'Number of women',
-        numberOfMen: 'Number of men',
-        min: true,
-        max: true,
-        avg: true,
-        count: true,
-        static: true,
-        first: true,
-      },
-      rowOrder: [
-        'first',
-        'numberOfWomen',
-        'numberOfMen',
-        'min',
-        'max',
-        'sum',
-        'avg',
-        'count',
-      ],
+  config: TableConfig = {};
+  ngOnInit() {
+    this.config = {
       columns: {
         firstName: {},
-        lastName: {
-          static: 'n/a',
-        },
-        gender: {
-          numberOfWomen: (data: Array<TableRow>, key) => {
-            let count = 0;
-            for (let i = 0; i < data.length; i++) {
-              if (data[i][key] === 'female') {
-                count++;
-              }
-            }
-            return count;
-          },
-          numberOfMen: (data: Array<TableRow>, key) => {
-            let count = 0;
-            for (let i = 0; i < data.length; i++) {
-              if (data[i][key] === 'male') {
-                count++;
-              }
-            }
-            return count;
-          },
-        },
+        lastName: {},
+        gender: {},
         favoriteFood: {
-          first: (data: Array<TableRow>, key) => data[0][key],
+          class: 'text-end flex-end',
         },
         age: {
-          sum: true,
-          avg: true,
-          count: true,
-          max: true,
-          min: true,
+          class: 'text-end',
         },
         weight: {
-          sum: true,
-          avg: true,
-          min: true,
+          class: 'text-end',
+          transform: {
+            pipe: DecimalPipe,
+            args: ['1.0-2'],
+          },
+        },
+        height: {
+          class: 'text-end',
+          templateRef: this.heightTmplRef,
         },
       },
-    },
-  };
+      footer: {
+        headers: {
+          sum: 'Total',
+          numberOfWomen: 'Number of women',
+          numberOfMen: 'Number of men',
+          min: true,
+          max: true,
+          avg: true,
+          count: true,
+          static: true,
+          first: true,
+        },
+        rowOrder: [
+          'first',
+          'numberOfWomen',
+          'numberOfMen',
+          'min',
+          'max',
+          'sum',
+          'avg',
+          'count',
+        ],
+        columns: {
+          firstName: {},
+          lastName: {
+            static: 'n/a',
+          },
+          gender: {
+            numberOfWomen: (data: Array<TableRow>, key) => {
+              let count = 0;
+              for (let i = 0; i < data.length; i++) {
+                if (data[i][key] === 'female') {
+                  count++;
+                }
+              }
+              return count;
+            },
+            numberOfMen: (data: Array<TableRow>, key) => {
+              let count = 0;
+              for (let i = 0; i < data.length; i++) {
+                if (data[i][key] === 'male') {
+                  count++;
+                }
+              }
+              return count;
+            },
+          },
+          favoriteFood: {
+            first: (data: Array<TableRow>, key) => data[0][key],
+          },
+          age: {
+            sum: true,
+            avg: true,
+            count: true,
+            max: true,
+            min: true,
+          },
+          weight: {
+            sum: true,
+            avg: true,
+            min: true,
+          },
+          height: {
+            avg: true,
+            min: true,
+            max: true,
+          },
+        },
+      },
+    };
+  }
+
 }`,
     language: 'typescript',
   },
